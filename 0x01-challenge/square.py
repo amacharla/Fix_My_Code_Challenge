0b9fbcd11199ced61fe0d4bc9d6a3d5c3c700e4f
Anoop M <noop2016@gmail.com>
2018-01-29 01:34:49 +0000

    idk why its not giving me checks i fixed it

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,10 +2,6 @@
 
 class square():
 
-    width = 0
-    height = 0
-
-
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -15,9 +11,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """print square """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
